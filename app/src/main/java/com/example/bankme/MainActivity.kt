d291fe1023de62a9cb8d8fbbd3487d28a3f9a9be
Jabakik <Jabakik@gmail.com>
2022-06-25 13:31:20 -0700

    Second commit

--- app/src/main/java/com/example/bankme/MainActivity.kt
+++ app/src/main/java/com/example/bankme/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Telephony
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val loginButton: Button = findViewById(R.id.bm_LogInButt)
+        val password: EditText = findViewById(R.id.bm_PhPass_In)
+        val pastext: TextView = findViewById(R.id.bm_PhPass)
+        val pasText1: TextView = findViewById(R.id.bm_PhPass1)
+
 
         loginButton.setOnClickListener {
+            if(password.text.toString()=="admin") {
         val intent = Intent(this,SecondActivity::class.java)
-        startActivity(intent)
+        startActivity(intent)}else{password.error}
     }
-
     }
 }
